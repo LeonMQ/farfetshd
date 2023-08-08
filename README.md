afd996585bd99714b74be7eb89fbce23caa6e395
LeonMQ <leon.kinsella@students.mq.edu.au>
2023-08-09 02:23:43 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,148 @@
+# Farfetshd
+
+## Sample output
+
+```
+
+ 🐧: Fedora 37 (6.3.12)
+ 🧠: i5-1240P 2.1GHz 35°C
+ 🐏: 3.0/8GB 38%
+ 💾: 19.3/254G 7.4% 30.8°C
+ 🔋: 99% Full battery
+ ⌚: 0d 10h 50m
+
+```
+
+## Description
+
+This is a shell script that prints out some system information, it is written in POSIX shell (I used dash).
+
+Made for linux, tested on Fedora workstation 37, please let me know what errors you have, and what distro you run.
+
+It only relies on 2 external commands (stat and awk), for the storage option. If you don't want storage, then no external commands are used. (*If someone knows a method to get this info without external commands it would be greatly appreciated*).
+
+## Installation
+
+Put farfetshd.sh somewhere in your path.
+
+## Configuration 
+
+The first function is config. In there you write what options you want to display, one per line.
+
+### Options
+
+* cpu
+
+    + Prints the name of the cpu, the frequency of core 0, and the temperature of the package
+
+* cpusmall
+
+    + Prints the name of the cpu
+
+* memory
+
+    + prints the gigbytes of RAM used and total, and the percentage used
+
+* memorysmall
+
+    + prints the total megabytes of ram
+
+* battery
+
+    + prints the percentage of battery, and the time remaining till empty or full charge
+
+* batterysmall
+
+    + prints the percentage of battery,
+
+* os
+
+    + prints the distribution name and version, and the kernel version
+
+* ossmall
+
+    + prints the distribution name
+
+* storage
+
+    + prints the gigabytes of storage used and total, the percentage used, and the tempurature of your nvme drive (if present)
+
+    + **Warning** this options significantly slows down the script (see [speed test results](#Results)
+
+* uptime
+
+    + prints the time your device has been on in days, hours, and minutes
+
+## Speed
+
+Speed testing was done by running:
+
+``` 
+
+#! /bin/sh
+time for run in {1..10000}; do
+    ./farfetshd.sh > /dev/null 2>&1;
+done
+
+```
+
+The real time was recorded.
+
+3 different configs were tested, each config was test 5 times.
+
+**Default** Config:
+
+``` 
+
+config(){
+os
+cpu
+memory
+storage
+battery
+uptime
+}
+
+```
+
+**NoStor** Config:
+
+``` 
+
+config(){
+os
+cpu
+memory
+battery
+uptime
+}
+
+```
+
+**Small** Config:
+
+``` 
+
+config(){
+ossmall
+cpusmall
+memorysmall
+batterysmall
+}
+
+```
+
+### Results
+
+|         | Default (s) | NoStor (s) | Small (s) |
+|---------|-------------|------------|-----------|
+| time 1  | 153.043     | 73.553     | 64.115    |
+| time 2  | 159.606     | 76.172     | 68.157    |
+| time 3  | 139.607     | 75.669     | 64.767    |
+| time 4  | 147.307     | 76.110     | 67.140    |
+| time 5  | 143.440     | 77.090     | 66.454    |
+| Average | 148.601     | 75.719     | 66.127    |
+
+## Inspiration
+
+This was heavily inspired by [dylanaraps/pfetch](https://github.com/dylanaraps/pfetch) and [6gk/fet.sh](https://github.com/6gk/fet.sh), and was helped significantly by [dylanaraps/pure-sh-bible](https://github.com/dylanaraps/pure-sh-bible) and numerous stackexchange and stackoverflow threads.
---
+++ farfetshd.sh
@@ -0,0 +1,216 @@
+!/bin/sh
+config(){
+os
+cpu
+memory
+battery
+uptime
+}
+assochw(){
+    if [ -n "$hwr" ]; then
+        return
+    fi
+    count=0
+    for find in /sys/class/hwmon/*; do
+        read -r content < $find/name
+        case $content in
+            *BAT*) bl=$find; ;;
+            *AC*) pl=$find; ;;
+            *nvme*) sl=$find; ;;
+            *core*) cl=$find; ;;
+            *k10*) cl=$find; ;;
+        esac
+        count=$((count+1))
+    done
+    hwr=1
+}
+cpu(){
+    if [ -z "$hwr" ]; then
+        assochw
+    fi
+    while  IFS=":" read -r line info; do
+        case $line in
+            model\ name*) cn=${info##*) }; ;;
+            cpu\ MHz*) cf=${info##* }; break;;
+        esac
+    done < /proc/cpuinfo
+    cf=${cf%%.*} # remove decimal from frequency
+    if [ ${#cf} = 4 ]; then
+        cf=${cf%??} #partial converion to GHz 
+        cfi=${cf%?}
+        cfd=$((cf%(cfi*10)))
+        cf="$cfi.$cfd""GHz"
+    else
+        cf="$cf""MHz"
+    fi
+    read -r ct < $cl/temp1_input
+    ct=${ct%%???}
+    printf " %s: %s %s %i%s\n" "🧠" "$cn" "$cf" "$ct" "°C"
+}
+cpusmall(){
+    while  IFS=":" read -r line info; do
+        case $line in
+            model\ name*) cn=${info##*) }; break;;
+        esac
+    done < /proc/cpuinfo
+    printf " %s: %s\n" "🧠" "$cn"
+}
+memory(){
+    while IFS=":" read -r name info; do
+        case $name in
+            MemTotal) mt=${info% *}; ;;
+            MemFree) mf=${info% *}; ;; # Usable RAM kibibyte
+            Cached) mc=${info% *}; ;; # RAM active in the system kibibyte
+            Buffers) mb=${info% *}; ;; # RAM active in the system kibibyte
+            SReclaimable) mr=${info% *}; ;; # RAM active in the system kibibyte
+        esac
+    done < /proc/meminfo
+    mu=$(($mt-$mf-$mb-$mc-$mr)) # get ram used in kibibytes
+    mp=$((($mu*100)/$mt)) # get usage percentage
+    mt=$((mt/976562)) #kibibytes to gigabytes
+    mu=$((mu/976)) #convert used ram from kibibytes to megabytes
+    mui=${mu%???}
+    if [ ${#mu} = 4 ]; then
+        mud=${mu%??}
+        mud=${mud#?}
+        mm="$mui.$mud/$mt""GB"
+    elif [ ${#mu} -le 3 ]; then
+        mm="$mu/$mt""GB"
+    else
+        mm="$mui/$mt""GB"
+    fi
+    printf " %s: %s %i%%\n" "🐏" "$mm" "$mp"
+}
+memorysmall(){    
+    while IFS=":" read -r line info; do
+        case $line in
+            MemTotal) mt=${info% *}; ;;
+        esac
+    done < /proc/meminfo 
+    mt=$((((mt/976)/1000)*1000))
+    printf " %s: %i%s\n" "🐏" "$mt" "MB"
+}
+battery(){
+    if [ -z "$hwr" ]; then
+        assochw
+    fi
+    while IFS="=" read -r line info; do
+        case $line in
+            POWER_SUPPLY_CHARGE_FULL) bcf=$info; ;; # The battery capacity in MicroAmpereHours
+            POWER_SUPPLY_CHARGE_NOW) bcn=$info; ;; # The present battery capacity in MicroAmpereHours
+            POWER_SUPPLY_CURRENT_NOW) bc=$info; ;; # The present outgoing charge in MicroAmperes
+            POWER_SUPPLY_POWER_NOW) bpn=$info; ;; # The present outgoing charge in MicroAmperes
+            POWER_SUPPLY_VOLTAGE_MIN_DESIGN) bvf=$info; ;; # The minimum battery outgoing voltage
+            POWER_SUPPLY_VOLTAGE_NOW) bvn=$info; ;; # The present outgoing battery voltage
+            POWER_SUPPLY_ENERGY_FULL) bef=$info; ;; # The battery capacity in microwatthours
+            POWER_SUPPLY_ENERGY_NOW) ben=$info; ;; # The present battery capacity in microwatthours
+            POWER_SUPPLY_CAPACITY) bp=$info; ;; # The present battery percentage
+        esac
+    done < $bl/device/uevent
+    while read -r line; do
+        case $line in
+            POWER_SUPPLY_ONLINE*) ps=${line#*=}; ;; # If the laptop is currently plugged in
+        esac
+    done < $pl/device/uevent
+    if [ "$bc" != "$bpn" ] && [ -z "$bc" ]; then
+        bc="$bpn"
+    fi
+    if [ -z "$bcf" -a "$bcn" ]; then
+        bef=${bef%???}
+        bvf=${bvf%???}
+        ben=${ben%???}
+        bvn=${bvn%???}
+        bcf=$((bef*bvf))
+        bcn=$((ben*bvn))
+    fi
+    bcl=$((99*bcf/100))
+    if [ -z "$bp" ]; then
+        bp=$((bcn/(bcf/100)))
+    fi
+    if [ "$bcn" -gt "$bcl" ]; then
+        if [ "$ps" = 1 ]; then
+            bs="Wall power"
+        else
+            bs="Full battery"
+        fi
+    else
+        if [ "$ps" = 0 ]; then
+            br=$((((bcn/(bc/1000))*600000)/10000000));
+            bh=$((br/60))
+            bm=$((br%60))
+            bs="$bh"":""$bm"" Remaining"
+        else
+            br=$(((((bcf-bcn)/(bc/1000))*600000)/10000000))
+            bh=$((br/60))
+            bm=$((br%60))
+            bs="$bh"":""$bm"" Till full charge"
+        fi
+    fi
+    printf " %s: %s%% %s\n" "🔋" "$bp" "$bs"
+}
+batterysmall(){
+    if [ -z "$hwr" ]; then
+        assochw
+    fi
+    while IFS="=" read -r line info; do
+        case $line in
+            POWER_SUPPLY_CAPACITY) bp=$info; ;; # The present battery percentage
+        esac
+    done < $bl/device/uevent
+    printf " %s: %i%%\n" "🔋" "$bp"
+}
+ossmall(){
+    while read -r line; do
+        case $line in
+            NAME=*) D=${line#*\"}; ;; #get distro name
+        esac
+    done < /usr/lib/os-release
+    D=${D%\"}
+    D=${D%% *} # shorten distro name
+    printf " %s: %s \n" "🐧" "$D"
+}
+os(){
+    while read -r line; do
+        case $line in
+            NAME=*) D=${line#*\"}; ;; #get distro name
+            VERSION_ID=*) V=${line#*=}; ;; #get distro version
+        esac
+    done < /usr/lib/os-release
+    D=${D%\"}
+    D=${D%% *} # shorten distro name
+    read -r K < /proc/version # get kernel information
+        K=${K%%-*} # shorten kernel name step 1
+        K=${K##* }  # shorten kernel name step 2
+        printf " %s: %s %s (%s)\n" "🐧" "$D" "$V" "$K"
+}
+storage(){
+    if [ -z "$hwr" ]; then
+        assochw
+    fi
+    s=$(stat -f / | awk '/^Blocks:/ {print (($3-$5)*4096),$3*4096}')
+    sr=${s##* } # Get total space of SSD
+    su=${s%% *} # get usued space of SSD
+    sr=${sr%?????????} # Convert total space to gigabytes approx
+    su=${su%???????} # Convert used space to 100s of mega bytes approx
+    sdr=1
+    sp=$(($su*10/$sr)) #  get space usage in per mille
+    spi=${sp%?} # get integer compoent of space usage percent
+    spd=$(($sp%$spi)) # get decimal  compoent of space usage percent
+    sug=${su%??}
+    sud=$(($su%($sug*100)))
+    if [ -n "$pl" ]; then
+        read -r st < $sl/temp1_input
+        sti=${st%%???}
+        std=${st##??}
+        std=${std%%??}
+        st="$sti"".""$std""°C"
+    else
+        st=""
+    fi
+    printf " %s: %i.%i/%iG %i.%i%% %s\n" "💾" "$sug" "$sud" "$sr" "$spi" "$spd" "$st"
+}
+uptime(){
+    IFS=. read -r U _ < /proc/uptime # get uptime in seconds
+    printf " %s: %id %ih %im\n" "⌚" "$(($U/60/60/24))" "$(($U/60/60%24))" "$(($U/60%60))"
+}
+config
